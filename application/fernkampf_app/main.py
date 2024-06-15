b12756231970aeb3eef73d4bb001531152cd997c
Ted <ted@ted.datenschmutz.space>
2024-06-15 18:22:10 +0200

    created first tkinter window with frame and button
    and fixed two little spelling errors

--- application/fernkampf_app/main.py
+++ application/fernkampf_app/main.py
@@ -1,7 +1,28 @@
 # here we will write part of the code
+import tkinter as tk
+
 
 def main():
-    print("Hello World!")
+    window = tk.Tk()
+    frameLeft = tk.Frame()
+    frameLeft.pack()
+    frameRight = tk.Frame()
+    frameRight.pack()
+    greeting = tk.Label(
+        text="This is the first test of our App!",
+        master=frameLeft,
+    )
+    greeting.pack()
+    button = tk.Button(
+        text="Please press here to start application!",
+        width=30,
+        height=1,
+        bg="grey",
+        fg="black",
+        master=frameRight,
+    )
+    button.pack()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
